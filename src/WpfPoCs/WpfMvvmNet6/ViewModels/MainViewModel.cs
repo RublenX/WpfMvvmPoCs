e1812510eb6f1b15f69afef13ac9ad92eb4fd5b1
J. Rubén Pérez Tejada <jrperez@ext.laligatech.com>
2023-02-25 01:42:03 +0100

    Se enlaza al paquete "CommunityToolkit.Mvvm" y se crea el primer ViewModel con su primera propiedad enlazable, para lo cual se necesita hacer la clase como "parcial" al crear el paquete su propio código fuente de respaldo

---
+++ src/WpfPoCs/WpfMvvmNet6/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WpfMvvmNet6.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string textoPrueba = "Esto es un texto de prueba";
+    }
+}
